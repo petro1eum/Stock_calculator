8280289319a47c338510707ebfbbfc532e5c6b36
EdCher <34724500+petro1eum@users.noreply.github.com>
2025-05-21 14:35:09 +0300

    publish

---
+++ stock-calculator/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import InventoryCalculator from './InventoryCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <InventoryCalculator />
+    </div>
+  );
+}
+
+export default App;
